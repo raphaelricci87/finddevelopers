59f886aa69b986e1857e85eb389ccd6d09d0d1b0
Santos <raphael.b.santos@capgemini.com>
2020-01-27 14:56:41 -0300

    backend

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://raphael:rapha123@cluster0-9gaw3.mongodb.net/Dev?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
